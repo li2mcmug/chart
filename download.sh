48a8a9ace64585d66ca560ece783324123e2e103
li2mcmug <pytong07@yahoo.com>
2013-04-30 19:35:20 -0700

    commit

--- download.sh
+++ download.sh
@@ -1,9 +1,10 @@
-symbols=("AA" "GE")
+symbols=("S")
 
 for i in {0..1}
 do
     symbol=${symbols[$i]}
     rm -rf temp.csv
+    rm -rf "data/${symbol}.csv"
     curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=${symbol}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
-    cat temp.csv | grep "2012-\|2013-" > "${symbol}.csv"
+    cat temp.csv | grep "2012-\|2013-" > "data/${symbol}.csv"
 done
