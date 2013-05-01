c437a4922d2e75ba70bf43c99da71f1e4e97bed7
li2mcmug <pytong07@yahoo.com>
2013-04-30 20:18:41 -0700

    more

--- download.sh
+++ download.sh
@@ -1,8 +1,9 @@
-symbols=("S")
+symbols=("AA" "S")
 
 for i in {0..1}
 do
     symbol=${symbols[$i]}
+    echo symbol
     rm -rf temp.csv
     rm -rf "data/${symbol}.csv"
     curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=${symbol}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
