982447e76a0e4c8d8c8ccb202411a60a76896767
li2mcmug <pytong07@yahoo.com>
2013-05-01 23:01:45 -0700

    can download single symbol now

--- download.sh
+++ download.sh
@@ -1,16 +1,26 @@
-FILE="config/symbol_list.txt"
+#! /bin/bash
 
+echo $1
 
-exec 0<"$FILE"
-n=0
-while read -r symbol
-do
-  rm -rf "data/${symbol}.csv"
-  curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=${symbol}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
-  cat temp.csv | grep "2012-\|2013-" > "data/${symbol}.csv"
+if [[ $1 ]]
+then
+  rm -rf "data/{$1}.csv"
+  curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s={$1}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
+  cat temp.csv | grep "2012-\|2013-" > "data/{$1}.csv"
 
-  rm -rf temp.csv
-done
+else
+  FILE="config/symbol_list.txt"
 
+  exec 0<"$FILE"
+  n=0
+  while read -r symbol
+  do
+    rm -rf "data/${symbol}.csv"
+    curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=${symbol}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
+    cat temp.csv | grep "2012-\|2013-" > "data/${symbol}.csv"
+
+   rm -rf temp.csv
+  done
+fi
 
 
