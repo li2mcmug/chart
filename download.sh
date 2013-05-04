25d6e4e6fed1e3d8455927f9e963bf791d7eca5d
li2mcmug <pytong07@yahoo.com>
2013-05-04 00:00:41 -0700

    erer

--- download.sh
+++ download.sh
@@ -4,9 +4,9 @@ echo $1
 
 if [[ $1 ]]
 then
-  rm -rf "data/{$1}.csv"
-  curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s={$1}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
-  cat temp.csv | grep "2012-\|2013-" > "data/{$1}.csv"
+  rm -rf "data/$1.csv"
+  curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=$1&a=00&b=1&c=2012&d=04&e=3&f=2013&g=d&ignore=.csv"
+  cat temp.csv | grep "2012-\|2013-" > "data/$1.csv"
 
 else
   FILE="config/symbol_list.txt"
@@ -16,7 +16,7 @@ else
   while read -r symbol
   do
     rm -rf "data/${symbol}.csv"
-    curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=${symbol}&a=00&b=1&c=2012&d=03&e=30&f=2013&g=d&ignore=.csv"
+    curl -o temp.csv "http://ichart.finance.yahoo.com/table.csv?s=${symbol}&a=00&b=1&c=2012&d=04&e=3&f=2013&g=d&ignore=.csv"
     cat temp.csv | grep "2012-\|2013-" > "data/${symbol}.csv"
 
    rm -rf temp.csv
