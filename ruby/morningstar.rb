ef2756ac31760a6453d958015dd1c5a05cfac33e
li2mcmug <li2mcmug@ubuntu.(none)>
2013-06-23 21:56:30 -0700

    Insert into database

--- ruby/morningstar.rb
+++ ruby/morningstar.rb
@@ -10,13 +10,16 @@ years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012]
 con = Mysql.new 'localhost', 'root', 'comp0707', 'stocks'
 
 begin
-  CSV.foreach("/home/li2mcmug/workspace/chart/morningstar/A.csv") do |row|
+  CSV.foreach("/home/li2mcmug/workspace/chart/morningstar/ZNGA.csv") do |row|
     break if row[0] && row[0].match("CAD")
 
-    next if row[0].nil? || !row[0].match("Revenue.*Mil")
+    next if row[0].nil? || !row[0].match("Revenue.*Mil") 
     for i in 1..10
-      rs = con.query "insert into symbols(symbol, year) values(\'A\',#{years[i-1]})"
-      puts row[i].sub(',', '')
+      next if row[i].nil?
+      row[i].sub!(',', '')
+      amount = row[i].to_i * 1000000
+      puts amount
+      rs = con.query "insert into symbols(symbol, year, revenue) values(\'ZNGA\',#{years[i-1]},#{amount})"
     end
   end
     
