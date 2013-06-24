d70efa4f36f54918602f16a36ca57645eb623a83
li2mcmug <li2mcmug@ubuntu.(none)>
2013-06-23 20:58:48 -0700

    insert into database

--- ruby/morningstar.rb
+++ ruby/morningstar.rb
@@ -5,19 +5,20 @@ require 'csv'
 require 'spreadsheet'
 
 Spreadsheet.client_encoding = 'UTF-8'
+years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012]
+
+con = Mysql.new 'localhost', 'root', 'comp0707', 'stocks'
 
 begin
   CSV.foreach("/home/li2mcmug/workspace/chart/morningstar/A.csv") do |row|
+    break if row[0] && row[0].match("CAD")
+
     next if row[0].nil? || !row[0].match("Revenue.*Mil")
-    for i in 1..11
+    for i in 1..10
+      rs = con.query "insert into symbols(symbol, year) values(\'A\',#{years[i-1]})"
       puts row[i].sub(',', '')
     end
   end
-
-  con = Mysql.new 'localhost', 'root', 'comp0707', 'stocks'
-  puts con.get_server_info
-  rs = con.query 'SELECT VERSION()'
-  puts rs.fetch_row    
     
 rescue Mysql::Error => e
   puts e.errno
