1394e7e588946572ca180e6c3664de5587c9637c
li2mcmug <li2mcmug@ubuntu.(none)>
2013-06-29 21:10:09 -0700

    more changes

--- ruby/morningstar.rb
+++ ruby/morningstar.rb
@@ -7,14 +7,19 @@ require 'spreadsheet'
 Spreadsheet.client_encoding = 'UTF-8'
 
 con = Mysql.new 'localhost', 'root', 'comp0707', 'stocks'
+csv_to_field = {}
+csv_to_field["EarningsPerShare"] = "eps"
 
 begin
   Dir.foreach("/home/li2mcmug/workspace/chart/morningstar") do |file|
     puts file
     years = nil
+    symbol = nil
 
     next if !file.match("csv") || File.directory?("/home/li2mcmug/workspace/chart/morningstar/#{file}")
 
+    year_values = {}
+  
     CSV.foreach("/home/li2mcmug/workspace/chart/morningstar/#{file}", { quote_char: "\x00"}) do |row|
       symbol = file.split(".")[0]
       row = row.join(',').gsub(/(?m),(?=[^"]*"(?:[^"\r\n]*"[^"]*")*[^"\r\n]*$)/,'')
@@ -26,19 +31,41 @@ begin
 
       break if (row[0] && row[0].match("CAD")) || file.split(".").size() > 2
 
-      next if row[0].nil? || !row[0].match("EarningsPerShare") 
+      next if row[0].nil? || csv_to_field.keys.select{|k| k if row[0].match(k)}.empty?
+      field_name = csv_to_field.each_pair{|k, v| v if row[0].match(k)}.first.last
 
+      years.each do |year|
+        year_values[year] = []
+      end
       for i in 1..10
         next if row[i].nil?
         row[i].sub!(',', '')
         row[i].sub!('"', '')
         amount = row[i].to_f
-
-        query = "insert into symbols(symbol, year, eps) values(\'#{symbol}\',#{years[i]},#{amount}) on duplicate key update eps = #{amount}"
-puts query
-        rs = con.query query
+        year_values[years[i]] << [field_name, amount]
       end
     end
+    
+    year_values.keys.each do |year|
+      next if year_values[year].empty?
+      insert_stmt = "insert into symbols(symbol, year"
+      value_stmt = " values(\'#{symbol}\',#{year}"
+      update_stmt = " on duplicate key update "
+      year_values[year].each do |field_value|
+	column_name = field_value.first
+        column_value = field_value.last
+        insert_stmt += ", #{column_name}"
+        value_stmt += ", #{column_value}"
+        update_stmt += "#{column_name} = #{column_value}, "
+      end
+      insert_stmt += ")"
+      value_stmt += ")"
+      update_stmt = update_stmt.chop.chop
+      query = insert_stmt + value_stmt + update_stmt
+      puts query
+      rs = con.query query
+    end    
+   
   end
 rescue Mysql::Error => e
   puts e.errno
